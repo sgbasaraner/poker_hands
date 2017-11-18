5697ae48769c1690d822b1c6fcae1aaa87cf751d
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-18 17:26:12 +0300

    separate user input by comma

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 	printf("Input the hand in this format: Ac, Tc, Kd, 3d, 7s\n");
 	char user_input[18];
-	fgets(user_input, 18, stdin);
-	printf("%s\n", user_input);
+	fgets(user_input, 19, stdin);
+
+	// separate user input by comma
+	const char separator[3] = ", ";
+   	char *token;
+   	token = strtok(user_input, separator);
+   	while (token != NULL) {
+    	printf("%s\n", token);
+      	token = strtok(NULL, separator);
+   	}
 }
