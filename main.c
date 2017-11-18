adcf57da55fb1a60f25470c2e27dbf7b07e431a0
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-18 17:09:40 +0300

    add primitive user input checking

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: ./main.c [5c, Ad, Tc, 9h, Ac]\n");
+		return 1;
+	}
 	char* user_input = argv[1];
 	printf("%s\n", user_input);
 }
